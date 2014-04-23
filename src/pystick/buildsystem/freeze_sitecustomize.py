e1e0fbf0214366ead6f2ac55130cfe247e2d7764
Tal Yalon <yalon@infinidat.com>
2014-04-23 14:55:32 +0300

    fixed our custom importer: since we don't know if a module is a package or not (we're not exposed to the frozen object size like Python does so we don't have its convention that -size is a package) we resort to putting packages under freezer_package.x.y.z while modules in freezer.x.y.z.

--- src/pystick/buildsystem/freeze_sitecustomize.py
+++ src/pystick/buildsystem/freeze_sitecustomize.py
@@ -1,15 +1,19 @@
 import imp
 import sys
 
+# The idea is that we put all the frozen modules under either freezer.xxx.yyy.zz or freezer_package.xxx.yy.zz,
+# depending on whether the module is a package or not (i.e. __init__). We do it in this hacky way because we're not
+# exposed to the size field of a frozen module so we can't know what Python's code knows that a module is a package
+# (negative size).
 class ModuleImporter(object):
     def find_module(self, fullname, path=None):
-        try:
-            imp.get_frozen_object("freezer." + fullname)
+        # Python's default import implementation doesn't handle builtins with '.' in them well, so we handle them here
+        # as well.
+        if imp.is_frozen('freezer_package.' + fullname) or \
+           imp.is_frozen('freezer.' + fullname) or \
+           (fullname.find('.') != -1 and imp.is_builtin(fullname)):
             return self
-        except ImportError:
-            if fullname.find('.') != -1 and imp.is_builtin(fullname):
-                # Python's default import implementation doesn't handle builtins with '.' in them well.
-                return self
+        else:
             return None
 
     def load_module(self, fullname):
@@ -29,13 +33,19 @@ class ModuleImporter(object):
                 freeze_external_modules.set_py_package_context(py_package_context)
                 imp.release_lock()
         else:
-            co = imp.get_frozen_object("freezer." + fullname)
+            if imp.is_frozen('freezer_package.' + fullname):
+                co = imp.get_frozen_object('freezer_package.' + fullname)
+                is_package = True
+            else:
+                co = imp.get_frozen_object("freezer." + fullname)  # This may throw ImportError if not found.
+                is_package = False
             try:
                 imp.acquire_lock()
                 mod = imp.new_module(fullname)
                 if '__builtins__' not in mod.__dict__:
                     mod.__dict__['__builtins__'] = __builtins__
-                mod.__path__ = fullname
+                if is_package:
+                    mod.__path__ = fullname
                 sys.modules[fullname] = mod
                 eval(co, mod.__dict__, mod.__dict__)
                 return mod
